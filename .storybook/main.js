666041e1ff85c32e167dc2e9220524c6aa0348b4
vinics <vinics.dr@gmail.com>
2022-10-13 13:03:13 -0300

    Storybook/main: End slash removed

--- .storybook/main.js
+++ .storybook/main.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinale: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system/'
+      config.base = '/design-system'
     }
 
     return config
